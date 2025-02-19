0dd4107a0eb6ff8ae72457646a6d1b6f1c5f621f
Piyush choudhary <piyush.gag6513@gmail.com>
2025-02-19 10:43:54 +0000

    pyxis

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Piyush Choudhary",
+  title: "Web3 Developer & Content Creator",
+  email: "piyush.gag6513@gmail.com",
+  gitHub: "https://github.com/Piyushbijarania",
+  instagram: "",
+  linkedIn: "",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "https://x.com/Piyushj8i",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
